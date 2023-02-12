3de1f8a6a72a4835cb2c8ac0ed16f855ead27da0
huntermarlette <huntermarlette@gmail.com>
2023-02-12 12:19:38 -0500

    Lab2 Initial Commit

---
+++ ECE492_Lab2/src/ChatRoomClient.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionListener;
+
+import javax.swing.SwingConstants;
+
+public class ChatRoomClient implements ActionListener, SwingConstants {
+
+	public ChatRoomClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
