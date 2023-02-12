eee338822183d20e746d1cebf04f86494760b199
huntermarlette <huntermarlette@gmail.com>
2023-02-12 12:27:24 -0500

    setup

--- ECE492_Lab2/src/ChatRoomClient.java
+++ ECE492_Lab2/src/ChatRoomClient.java
@@ -1,16 +1,29 @@
-import java.awt.event.ActionListener;
 
-import javax.swing.SwingConstants;
 
-public class ChatRoomClient implements ActionListener, SwingConstants {
 
-	public ChatRoomClient() {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+//import java.awt.event.ActionEvent;
+
+public class ChatRoomClient implements ActionListener 
+{
+
+	public ChatRoomClient() // My constructor method
+	{
 		// TODO Auto-generated constructor stub
-	}
+		
+	} // end of constructor
 
-	public static void main(String[] args) {
+	public void actionPerformed(ActionEvent ae)	// buttons etc will call here
+	{	
+		
+	} // end of Actions Performed
+	
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
-	}
+	} // end of main
 
-}
+} // end of class 
