d903bc635eed76d744006a098aa8d22df2c2539e
huntermarlette <huntermarlette@gmail.com>
2023-02-19 20:11:52 -0500

    Catching Exceptions v1 code

--- ECE492ClassroomExercises/Lecture4_SayHelloTo.java
+++ ECE492ClassroomExercises/Lecture4_SayHelloTo.java
@@ -9,15 +9,31 @@ public class Lecture4_SayHelloTo {
 	}
 	
 	
-	public static void main (String[] args) 
-	{
-		if (args.length == 0) // no command line parameter
-	     {
-	     System.out.println("Restart. Provide names to say 'hello' to as command line parameters!");
-	     return;
-	     }
-	  for (int i = 0; i < args.length; i++) // print all names that were entered as parms.
-	       System.out.println("Hello, " + args[i]);
-	}
+//	public static void main (String[] args) 
+//	{
+//		if (args.length == 0) // no command line parameter
+//	     {
+//	     System.out.println("Restart. Provide names to say 'hello' to as command line parameters!");
+//	     return;
+//	     }
+//	  for (int i = 0; i < args.length; i++) // print all names that were entered as parms.
+//	       System.out.println("Hello, " + args[i]);
+//	} // end of main
+	
+	
+	public static void main(String[] args)
+	  {
+	  try {
+	      for (int i = 0; i < args.length; i++) 
+	           System.out.println("Hello, " + args[i]);
+	      }
+	  catch (ArrayIndexOutOfBoundsException exp)
+	      {
+	      System.out.println("Please restart. Provide the names of people to say 'hello' to as command line parameters.");
+	      return; // terminate by returning-to-caller (the CommandLineLoader)
+	      }
+	  System.out.println("Have a nice day!");
+	  }	 // end of main
+	
 
-}
+} // end of class
