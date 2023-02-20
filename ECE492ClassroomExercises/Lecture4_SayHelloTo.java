45cd294ba929c6d1ee9497384aef8c2891047505
huntermarlette <huntermarlette@gmail.com>
2023-02-19 20:09:54 -0500

    added main function and SayHelloTo code

--- ECE492ClassroomExercises/Lecture4_SayHelloTo.java
+++ ECE492ClassroomExercises/Lecture4_SayHelloTo.java
@@ -1,8 +1,23 @@
 
+// there was no code written in this actual lecture but the lecture document did have one
+
 public class Lecture4_SayHelloTo {
 
-	public Lecture4_SayHelloTo() {
+	public Lecture4_SayHelloTo() 
+	{
 		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public static void main (String[] args) 
+	{
+		if (args.length == 0) // no command line parameter
+	     {
+	     System.out.println("Restart. Provide names to say 'hello' to as command line parameters!");
+	     return;
+	     }
+	  for (int i = 0; i < args.length; i++) // print all names that were entered as parms.
+	       System.out.println("Hello, " + args[i]);
 	}
 
 }
