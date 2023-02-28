49c4c7f2c32c0916e77d349f97f72632ee24ad88
huntermarlette <huntermarlette@gmail.com>
2023-02-27 19:49:36 -0500

    Part 3 Send chat messages complete

--- ECE492_Lab2/src/ChatRoomClient.java
+++ ECE492_Lab2/src/ChatRoomClient.java
@@ -54,7 +54,7 @@ public class ChatRoomClient extends JFrame implements ActionListener
 		{
 		    throw new IllegalArgumentException("Parameters may not contain blanks."); // also returns.
 		}
-		System.out.println("bookmark 3");
+		System.out.println("bookmark 5");
 		
 		System.out.println("Connecting to the chat room server at " + serverAddress + " on port 2222.");
 		s = new Socket(serverAddress, 2222); // connect (server net address is specified by user but server port # is assumed)
@@ -72,6 +72,7 @@ public class ChatRoomClient extends JFrame implements ActionListener
 		} else { 
 			   throw new IllegalArgumentException("Join of " + clientName + " with password " + password + " was not successful."); // also returns
 		}
+		
 		
 		//COMPOSE the GUI window (Add the GUI objects to the window in memory)
 		topPanel.setLayout(new GridLayout(1,3)); // a format with 1 row and 3 columns
@@ -106,52 +107,105 @@ public class ChatRoomClient extends JFrame implements ActionListener
 	    receiveChatArea.setEditable(false); // keep user from changing the output area!
 	    errMsgTextField.setEditable(false); // keep user from changing the error message area!
 	    
-	    System.out.println("bookmark 4");
+	    System.out.println("bookmark 6");
 	    
 	    sendPublicButton.addActionListener(this); // sendPublicButton can now call us when the user pushes it!                                      
         // "this" is our program's address in memory (different every run)
 	    
-	    UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel"); //Add this line to help Mac GUIs look better:
+	    //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel"); //Add this line to help Mac GUIs look better:
 	    // Alternative Method:
-//	    chatWindow.setMenuBar(menuBar);
-//	    menuBar.add(fontMenu);
-//	    fontMenu.add(biggerFontMenuItem);
-//	    fontMenu.add(smallerFontMenuItem);
-//	    biggerFontMenuItem.addActionListener(this); // so these FontMenuItems (buttons!) 
-//	    smallerFontMenuItem.addActionListener(this);// in the font menu can call us!
+	    chatWindow.setMenuBar(menuBar);
+	    menuBar.add(fontMenu);
+	    fontMenu.add(biggerFontMenuItem);
+	    fontMenu.add(smallerFontMenuItem);
+	    biggerFontMenuItem.addActionListener(this); // so these FontMenuItems (buttons!) 
+	    smallerFontMenuItem.addActionListener(this);// in the font menu can call us!
 	    
 	    chatWindow.setSize(800,500);    // width,height
 	    chatWindow.setLocation(400,0);  // x,y (x is "to the right of the left margin", y is "down-from-the-top")
 	    chatWindow.setVisible(true);    // show it
 	    chatWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // terminate if user closes window
 	    
-	    
+	    System.out.println("bookmark 7");
 	} // end of constructor
 
 	
-	@Override
+	//@Override
 	public void actionPerformed(ActionEvent ae)	// buttons etc will call here
 	{	
+		errMsgTextField.setText("");                // clear any error message
+		errMsgTextField.setBackground(Color.white); // and remove highlight
+		
+		if (ae.getSource() == sendPublicButton) // is the sendPublicButton calling us?
+		   {
+		   String chatMessageToSend = sendChatArea.getText().trim(); // remove leading/trailing blanks.
+		   if (chatMessageToSend.length() == 0) // a blank message!
+		      {
+		      errMsgTextField.setText("No message entered to send.");
+		      errMsgTextField.setBackground(Color.pink); // highlight to get attention
+		      return; // return button's thread to the button.
+		      }
+		   //System.out.println("Your message '" + chatMessageToSend + "' is being sent to the server.");
+		   //sendChatArea.setText(""); // clear the input field.(indication to user that the message was sent.)
+		   
+		   // Send() Function?
+		   try {
+			   oos.writeObject(chatMessageToSend);
+			   sendChatArea.setText(""); // clear input area
+			   }
+		   catch(Exception e) // Uh-oh - the network or the server is down!
+			   { 
+			   // Take no action.
+			   // If the connection to the server is down, the readObject() method in the ois has
+			   // no-doubt already seen it and the catch block in receive() is telling the user they
+			   // must restart the client to re-establish connection with the chat server. 
+			   } 
+		   return; // return the button's thread to the JButton program
+		   } // end of processing block for the send button
+
+		
+		
+		
+		if (ae.getSource() == biggerFontMenuItem)
+		   { // This button increases the font size in the in and out chatTextAreas
+		   if (fontSize < maxFontSize) fontSize += 1;
+		      {
+		      sendChatArea.setFont(new Font("default", Font.BOLD, fontSize)); 
+		      receiveChatArea.setFont(new Font("default", Font.BOLD,fontSize));
+		      }
+		   return;   
+		   }
+
+		if (ae.getSource() == smallerFontMenuItem)
+		   { // This button reduces the font size in the in and out chatTextAreas
+		   if (fontSize > minFontSize) fontSize -= 1;
+		      {
+		      sendChatArea.setFont(new Font("default", Font.BOLD, fontSize)); 
+		      receiveChatArea.setFont(new Font("default", Font.BOLD,fontSize));
+		      }
+		   return;   
+		   }
+		
 		
 	} // end of Actions Performed
 	
 	
 	public static void main(String[] args) throws Exception 
 	{
-		//System.out.println("bookmark 0");
+		//System.out.println("bookmark 1");
 		// I could shorten these if statements into single line commands
 		if (args.length !=3) 
 		{
 			System.out.println("Restart. Provide 3 command line parameters: ServerAddress ChatName password");
 		}
-		//System.out.println("bookmark 0.5");
+		//System.out.println("bookmark 2");
 		if (args.length ==3) 
 		{
-			//System.out.println("bookmark 1");
+			//System.out.println("bookmark 3");
 			try {
-				System.out.println("bookmark 1.5");
+				System.out.println("bookmark 4");
 			    ChatRoomClient crc = new ChatRoomClient(args[0], args[1], args[2]); // "new" calls the ObjectLoader
-			    System.out.println("bookmark 2");
+			    System.out.println("bookmark 0000");
 			    // crc.receive(); // branch the main thread into the program object's receive() method to
 			    }                 // receive & display chat messages received from the server (other clients).
 			catch(Exception e)
