97c123916a3a070f30dead87c244c7faae8c79e7
huntermarlette <huntermarlette@gmail.com>
2023-02-27 20:48:44 -0500

    almost finished, not gone through rubric yet

--- ECE492_Lab2/src/ChatRoomClient.java
+++ ECE492_Lab2/src/ChatRoomClient.java
@@ -6,11 +6,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
 import java.io.*;
 import java.net.Socket;
-
-//import java.awt.event.ActionEvent;
 
 public class ChatRoomClient extends JFrame implements ActionListener 
 {
@@ -24,7 +21,7 @@ public class ChatRoomClient extends JFrame implements ActionListener
 	int minFontSize 		= 5;
 	
 	// GUI Objects
-	JFrame      chatWindow          = new JFrame(); // like int x = 5; to provide an inital value to the pointer field
+	JFrame      chatWindow          = new JFrame(); 				// like int x = 5; to provide an inital value to the pointer field
 	JPanel      topPanel            = new JPanel();
 	JPanel      middlePanel         = new JPanel();
 	JPanel      bottomPanel         = new JPanel();
@@ -35,7 +32,7 @@ public class ChatRoomClient extends JFrame implements ActionListener
 	JButton     availableButton     = new JButton();
 	JTextField  errMsgTextField     = new JTextField("Error messages will show here.");
 	JTextArea   sendChatArea        = new JTextArea();
-	JList<String> whosInList        = new JList<String>(); // note JList is declared to hold String objects (chat names)
+	JList<String> whosInList        = new JList<String>(); 			// note JList is declared to hold String objects (chat names)
 	JTextArea   receiveChatArea     = new JTextArea();
 	JScrollPane sendScrollPane      = new JScrollPane(sendChatArea);
 	JScrollPane whosInScrollPane    = new JScrollPane(whosInList); 
@@ -48,36 +45,33 @@ public class ChatRoomClient extends JFrame implements ActionListener
 	MenuItem smallerFontMenuItem = new MenuItem("Smaller");
     
     
-	public ChatRoomClient(String serverAddress, String clientName, String password) throws Exception // My constructor method
+	public ChatRoomClient(String serverAddress, String clientName, String password) throws Exception  // My constructor method
 	{
 		if (serverAddress.contains(" ") || clientName.contains(" ") || password.contains(" "))
-		{
 		    throw new IllegalArgumentException("Parameters may not contain blanks."); // also returns.
-		}
-		//System.out.println("bookmark 5");
-		
+			
+		System.out.println("Welcome to Lab 2 - ChatRoomClient by Hunter Marlette");
 		System.out.println("Connecting to the chat room server at " + serverAddress + " on port 2222.");
-		s = new Socket(serverAddress, 2222); // connect (server net address is specified by user but server port # is assumed)
+		s = new Socket(serverAddress, 2222); 						// connect (server net address is specified by user but server port # is assumed)
 		
 		System.out.println("Connected to the chat server!");
-		
-		oos = new ObjectOutputStream(s.getOutputStream()); // oos is declared as an instance variable.
-		oos.writeObject(clientName + " " + password);        // send the "join message" (a String)
-		ois = new ObjectInputStream(s.getInputStream());   // ois is declared as an instance variable.
-		String reply = (String) ois.readObject();          // wait for server response (note cast of receive type from Object to String)
+		oos = new ObjectOutputStream(s.getOutputStream()); 			// oos is declared as an instance variable.
+		oos.writeObject(clientName + " " + password);        		// send the "join message" (a String)
+		ois = new ObjectInputStream(s.getInputStream());   			// ois is declared as an instance variable.
+		String reply = (String) ois.readObject();          			// wait for server response (note cast of receive type from Object to String)
 		
 		if (reply.startsWith("Welcome")) 
-		{
+			{
 			   System.out.println("Join was successful!");
-		} else { 
+			} else { 
 			   throw new IllegalArgumentException("Join of " + clientName + " with password " + password + " was not successful."); // also returns
-		}
+			}
 		
 		//COMPOSE the GUI window (Add the GUI objects to the window in memory)
-		topPanel.setLayout(new GridLayout(1,3)); // a format with 1 row and 3 columns
-	    topPanel.add(sendMsgLabel);              // goes in row 1 column 1
-	    topPanel.add(whosInLabel);               // goes in row 1 column 2
-	    topPanel.add(receivedMsgsLabel);         // goes in row 1 column 3
+		topPanel.setLayout(new GridLayout(1,3)); 					// a format with 1 row and 3 columns
+	    topPanel.add(sendMsgLabel);              					// goes in row 1 column 1
+	    topPanel.add(whosInLabel);               					// goes in row 1 column 2
+	    topPanel.add(receivedMsgsLabel);        					// goes in row 1 column 3
 	    chatWindow.getContentPane().add(topPanel,"North");
 
 	    middlePanel.setLayout(new GridLayout(1,3)); 
@@ -88,7 +82,7 @@ public class ChatRoomClient extends JFrame implements ActionListener
 
 	    bottomPanel.setLayout(new GridLayout(1,3)); 
 	    bottomPanel.add(sendPublicButton);
-	    bottomPanel.add(availableButton); // anticipate some future function
+	    bottomPanel.add(availableButton); 							// anticipate some future function
 	    bottomPanel.add(errMsgTextField);
 	    chatWindow.getContentPane().add(bottomPanel,"South");
 	    
@@ -97,119 +91,109 @@ public class ChatRoomClient extends JFrame implements ActionListener
 	    sendPublicButton.setBackground(Color.green);
 	    whosInLabel.setForeground(Color.blue);
 	    
-	    receiveChatArea.setLineWrap(true);     // cause long text added to be properly
-	    receiveChatArea.setWrapStyleWord(true);// "wrapped" to the next line.
+	    receiveChatArea.setLineWrap(true);     						// cause long text added to be properly
+	    receiveChatArea.setWrapStyleWord(true);						// "wrapped" to the next line.
 	    sendChatArea.setLineWrap(true);
 	    sendChatArea.setWrapStyleWord(true);
 	    
-	    receiveChatArea.setEditable(false); // keep user from changing the output area!
-	    errMsgTextField.setEditable(false); // keep user from changing the error message area!
+	    receiveChatArea.setEditable(false); 						// keep user from changing the output area!
+	    errMsgTextField.setEditable(false); 						// keep user from changing the error message area!
 	    
-	    //System.out.println("bookmark 6");
-	    
-	    sendPublicButton.addActionListener(this); // sendPublicButton can now call us when the user pushes it!                                      
-        // "this" is our program's address in memory (different every run)
-	    
+	    sendPublicButton.addActionListener(this); 					// sendPublicButton can now call us when the user pushes it!                                      
+        															// "this" is our program's address in memory (different every run)
 	    //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel"); //Add this line to help Mac GUIs look better:
 	    // Alternative Method:
 	    chatWindow.setMenuBar(menuBar);
 	    menuBar.add(fontMenu);
 	    fontMenu.add(biggerFontMenuItem);
 	    fontMenu.add(smallerFontMenuItem);
-	    biggerFontMenuItem.addActionListener(this); // so these FontMenuItems (buttons!) 
-	    smallerFontMenuItem.addActionListener(this);// in the font menu can call us!
+	    biggerFontMenuItem.addActionListener(this); 				// so these FontMenuItems (buttons!) 
+	    smallerFontMenuItem.addActionListener(this); 				// in the font menu can call us!
 	    
-	    chatWindow.setSize(800,500);    // width,height
-	    chatWindow.setLocation(400,0);  // x,y (x is "to the right of the left margin", y is "down-from-the-top")
-	    chatWindow.setVisible(true);    // show it
-	    chatWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // terminate if user closes window
-	    
-	    System.out.println("bookmark 7");
+	    chatWindow.setSize(800,500);    							// width,height
+	    chatWindow.setLocation(400,0);  							// x,y (x is "to the right of the left margin", y is "down-from-the-top")
+	    chatWindow.setVisible(true);    							// show it
+	    chatWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 	// terminate if user closes window
 	} // end of constructor
 
-	// adding a new method for receive() method:
+	//receive() method:
 	public void receive() 
 	{
 		try {
-			while(true) // capture entering main thread in a do-forever read loop
-		    {
-				Object incomingMessage = ois.readObject(); // WAIT for a message from the server
+			while(true) 											// capture entering main thread in a do-forever read loop
+		    	{
+				Object incomingMessage = ois.readObject(); 			// WAIT for a message from the server
 				
 				// if you get a string type
 				if (incomingMessage instanceof String)
-				{
-					String receivedChatMessage = (String) incomingMessage; // Copy and CAST pointer 
+					{
+					String receivedChatMessage = (String) incomingMessage; 		// Copy and CAST pointer 
 					// The received object is now also pointed to by the pointer receivedChatMessage which is type String 
 					// Pass the String pointer to methods that expect to receive a String as a parameter, or
 					// call String methods on the received object using the String pointer.
 					receiveChatArea.append(newLine + receivedChatMessage);  // show text on GUI 
 					// auto-scroll the JScrollPane to bottom line so the last message will be visible.
 					receiveChatArea.setCaretPosition(receiveChatArea.getDocument().getLength()); // scroll to bottom
-				}
+					}
 				
 				// if you get an array of strings:
 				if (incomingMessage instanceof String)
-		        {
-					String receivedChatMessage = (String) incomingMessage; // Copy and CAST pointer 
+		        	{
+					String receivedChatMessage = (String) incomingMessage; 		// Copy and CAST pointer 
 					//...
-		        }
-				else if (incomingMessage instanceof String[]) // an array of Strings 
-		        {
-					String[] listOfWhosIn = (String[]) incomingMessage; // Copy and CAST pointer 
-					whosInList.setListData(listOfWhosIn); // give JList the array of Strings to display on GUI.
-		        }
-				else if (incomingMessage instanceof ImageIcon) // a picture! 
-		        {
-					ImageIcon receivedPicture = (ImageIcon) incomingMessage; // Copy and CAST pointer 
+		        	}
+				else if (incomingMessage instanceof String[]) 					// an array of Strings 
+		        	{
+					String[] listOfWhosIn = (String[]) incomingMessage; 		// Copy and CAST pointer 
+					whosInList.setListData(listOfWhosIn); 						// give JList the array of Strings to display on GUI.
+		        	}
+				else if (incomingMessage instanceof ImageIcon) 					// a picture! 
+		        	{
+					ImageIcon receivedPicture = (ImageIcon) incomingMessage; 	// Copy and CAST pointer 
 					System.out.println("A picture was received from the server. " + receivedPicture);
 					System.out.println("Pictures are not processed by the lab 2 ChatRoomClient.");
-		        }
-				else    // Ignore unexpected object type:    
-		        {
+		        	}
+				else    														// Ignore unexpected object type:    
+		        	{
 					System.out.println("Unexpected object type received from server: " + incomingMessage); // unrecognized object will print itself. 
-		        }
-		    }
+		        	}
+		    	}
 			}
 		catch(Exception e) 
 			{
 			// show error message to user
-		    errMsgTextField.setBackground(Color.pink); // this will get their attention!
+		    errMsgTextField.setBackground(Color.pink); 							// this will get their attention!
 		    errMsgTextField.setText("CHAT ROOM SERVER CONNECTION HAS FAILED!");
 		    receiveChatArea.append(newLine + "You must close this chatWindow and then restart the client to reconnect to the server to continue.");
 		    receiveChatArea.setCaretPosition(receiveChatArea.getDocument().getLength()); // scroll down
 		    // disable the GUI function
-		    sendChatArea.setEditable(false); // keep user from trying to send any more messages.
-		    sendPublicButton.setEnabled(false);    // stop button pushing
-			
-			}
-		// receive thread, now out of the loop, will return to the main() method and be terminated.
-	}
+		    sendChatArea.setEditable(false); 									// keep user from trying to send any more messages.
+		    sendPublicButton.setEnabled(false);    								// stop button pushing
+			} // receive thread, now out of the loop, will return to the main() method and be terminated.
+	} // end of receive method
 	
 	
-	
-	
-	//@Override
-	public void actionPerformed(ActionEvent ae)	// buttons etc will call here
-	{	
-		errMsgTextField.setText("");                // clear any error message
-		errMsgTextField.setBackground(Color.white); // and remove highlight
+	public void actionPerformed(ActionEvent ae)						// buttons etc will call here
+		{	
+		errMsgTextField.setText("");                				// clear any error message
+		errMsgTextField.setBackground(Color.white); 				// and remove highlight
 		
-		if (ae.getSource() == sendPublicButton) // is the sendPublicButton calling us?
+		if (ae.getSource() == sendPublicButton) 					// is the sendPublicButton calling us?
 		   {
-		   String chatMessageToSend = sendChatArea.getText().trim(); // remove leading/trailing blanks.
-		   if (chatMessageToSend.length() == 0) // a blank message!
+		   String chatMessageToSend = sendChatArea.getText().trim();// remove leading/trailing blanks.
+		   if (chatMessageToSend.length() == 0) 					// a blank message!
 		      {
 		      errMsgTextField.setText("No message entered to send.");
-		      errMsgTextField.setBackground(Color.pink); // highlight to get attention
-		      return; // return button's thread to the button.
+		      errMsgTextField.setBackground(Color.pink); 			// highlight to get attention
+		      return; 												// return button's thread to the button.
 		      }
 		   System.out.println("Your message '" + chatMessageToSend + "' is being sent to the server.");
-		   //sendChatArea.setText(""); // clear the input field.(indication to user that the message was sent.)
-		   try {	// Send() Function?
+		   //sendChatArea.setText(""); 								  // clear the input field.(indication to user that the message was sent.)
+		   try {													// Send() Function?
 			   oos.writeObject(chatMessageToSend);
-			   sendChatArea.setText(""); // clear input area
+			   sendChatArea.setText(""); 							// clear input area
 			   }
-		   catch(Exception e) // Uh-oh - the network or the server is down!
+		   catch(Exception e) 										// Uh-oh - the network or the server is down!
 			   { 
 			   // Take no action.
 			   // If the connection to the server is down, the readObject() method in the ois has
@@ -220,9 +204,8 @@ public class ChatRoomClient extends JFrame implements ActionListener
 		   } // end of processing block for the send button
 
 		
-		
-		if (ae.getSource() == biggerFontMenuItem)
-		   { // This button increases the font size in the in and out chatTextAreas
+		if (ae.getSource() == biggerFontMenuItem) 					// This button increases the font size in the in and out chatTextAreas
+		   { 
 		   if (fontSize < maxFontSize) fontSize += 1;
 		      {
 		      sendChatArea.setFont(new Font("default", Font.BOLD, fontSize)); 
@@ -231,8 +214,8 @@ public class ChatRoomClient extends JFrame implements ActionListener
 		   return;   
 		   }
 
-		if (ae.getSource() == smallerFontMenuItem)
-		   { // This button reduces the font size in the in and out chatTextAreas
+		if (ae.getSource() == smallerFontMenuItem)					// This button reduces the font size in the in and out chatTextAreas
+		   { 
 		   if (fontSize > minFontSize) fontSize -= 1;
 		      {
 		      sendChatArea.setFont(new Font("default", Font.BOLD, fontSize)); 
@@ -240,42 +223,29 @@ public class ChatRoomClient extends JFrame implements ActionListener
 		      }
 		   return;   
 		   }
-		
+
 	} // end of Actions Performed
-	
-	
-	
 	
 	
 	public static void main(String[] args) throws Exception 
 	{
-		//System.out.println("bookmark 1");
-		// I could shorten these if statements into single line commands
 		if (args.length !=3) 
-		{
 			System.out.println("Restart. Provide 3 command line parameters: ServerAddress ChatName password");
-		}
-		//System.out.println("bookmark 2");
+		
 		if (args.length ==3) 
-		{
-			//System.out.println("bookmark 3");
+			{
 			try {
-				System.out.println("bookmark 4");
-			    ChatRoomClient crc = new ChatRoomClient(args[0], args[1], args[2]); // "new" calls the ObjectLoader
-			    crc.receive(); // branch the main thread into the program object's receive() method to
-			    System.out.println("bookmark 0000");
-			    }                 // receive & display chat messages received from the server (other clients).
+			   	ChatRoomClient crc = new ChatRoomClient(args[0], args[1], args[2]); // "new" calls the ObjectLoader
+			   	crc.receive(); 														// branch the main thread into the program object's receive() method to
+			    }                 													//receive & display chat messages received from the server (other clients).
 			catch(Exception e)
 			    {   
-			    System.out.println(e); // print the exception object as the error message
-			    
-			    System.out.println("Connection to ChatRoomServer at " + args[0] + " on port 2222 has failed.");
-			    System.out.println("Is the server started? Have you entered the correct network address for the server?"); 
-			    
-			    return; // can't continue if can't load the program!
-			    }       // (OR we have encountered an Exception in the receive() method during the receive() process.)
-		}
-		
+			   	System.out.println(e); 												// print the exception object as the error message
+		    	System.out.println("Connection to ChatRoomServer at " + args[0] + " on port 2222 has failed.");
+		    	System.out.println("Is the server started? Have you entered the correct network address for the server?"); 
+		    	return; 															// can't continue if can't load the program!
+			    }       															//(OR we have encountered an Exception in the receive() method during the receive() process.)
+			}
 	} // end of main
 
 } // end of class 
