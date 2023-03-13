f43150ab6b3ca66cdba5f9c0ea0679e766f569a3
huntermarlette <huntermarlette@gmail.com>
2023-03-13 03:39:52 -0400

    first commit?

---
+++ ECE492_Lab3/src/ChatRoomServer.java
@@ -0,0 +1,32 @@
+
+// ECE492 Java Lab 3 - Chat Room Server - Spring 2023
+// Hunter Marlette
+// Student ID # 200289830
+
+// Note:
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+
+public class ChatRoomServer extends JFrame implements ActionListener 
+{
+	public ChatRoomServer() throws Exception  	// My constructor method
+	{
+		
+	} // end of constructor method
+	
+	public void actionPerformed(ActionEvent ae) 
+	{
+		
+	} // end of actionsPerformed
+	
+	public static void main(String[] args) throws Exception 
+	{
+		
+	} //end of main
+	
+} // end of class
