7032f1ac562c8829a722fb09bc62bfe8e94eb262
huntermarlette <huntermarlette@gmail.com>
2023-03-15 07:37:21 -0400

    Created Reporter Class

---
+++ ECE492_Lab4/src/Reporter.java
@@ -0,0 +1,31 @@
+
+// ECE492 Java Lab 4 - News Conference - Spring 2023
+	// class 2 - Reporter Class
+// Hunter Marlette
+// Student ID # 200289830
+
+// Note:
+
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.Socket;
+
+
+public class Reporter implements Runnable {
+
+	public Reporter() 							// Constructor Method
+	{
+		
+	} // end of constructor
+
+	public void run() 							// Run() Method
+	{
+		
+	} // end of run()
+	
+} // end of class
