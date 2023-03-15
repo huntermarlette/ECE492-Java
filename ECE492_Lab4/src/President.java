79f42abad078589564517e8e79077fa2e74c11af
huntermarlette <huntermarlette@gmail.com>
2023-03-15 10:58:18 -0400

    changed try/catch structure

--- ECE492_Lab4/src/President.java
+++ ECE492_Lab4/src/President.java
@@ -41,22 +41,39 @@ public class President implements Runnable {
 		
 		System.out.println("Reminder: The phrase 'God Bless America' ends the news conference.");
 		System.out.println("Enter a statement:");
-		
-		while(true) 
-		{
-			String statement = br.readLine();
-			if(statement.equals("God bless America") || statement.equals("God Bless America") || statement.equals("god bless america") || statement.equals("GOD BELSS AMERICA")) break;
-			if (statement.equals("") || statement.isEmpty()) 							// checks for blank message
+// try/catch structure option 1:
+		try {
+			while(true) 
 			{
-				System.out.println("Blank messages are ignored.");
-			} else {																		// proceed if message is not blank
-				try{
-					whiteHouse.makeAstatement(statement);
-				} catch(IOException e) {					// THIS IS NOT WHAT HE ASKED FOR BUT I DO NOT KNOW WHAT TO DO OTHERWISE!!!
-					break;
-				}
-			} // end of if/else statement
-		} // end of while loop
+				String statement = br.readLine();
+				if(statement.equals("God bless America") || statement.equals("God Bless America") || statement.equals("god bless america") || statement.equals("GOD BELSS AMERICA")) break;
+				if (statement.equals("") || statement.isEmpty()) 							// checks for blank message
+				{
+					System.out.println("Blank messages are ignored.");
+				} else {																		// proceed if message is not blank
+					whiteHouse.makeAstatement(statement);							
+				} // end of if/else statement
+			} // end of while loop
+		} catch(IOException e) {
+			System.out.println("IOException - Possible Keyboard Failure");
+		}
+		
+// try/catch structure option 2:
+//		while(true) 
+//		{
+//			String statement = br.readLine();
+//			if(statement.equals("God bless America") || statement.equals("God Bless America") || statement.equals("god bless america") || statement.equals("GOD BELSS AMERICA")) break;
+//			if (statement.equals("") || statement.isEmpty()) 							// checks for blank message
+//			{
+//				System.out.println("Blank messages are ignored.");
+//			} else {																		// proceed if message is not blank
+//				try{
+//					whiteHouse.makeAstatement(statement);
+//				} catch(IOException e) {					// THIS IS NOT WHAT HE ASKED FOR BUT I DO NOT KNOW WHAT TO DO OTHERWISE!!!
+//					break;									// ???
+//				}											// how do I have the try block inside the loop but put the catch block outside the loop?!? That Makes no sense!
+//			} // end of if/else statement
+//		}
 		
 		System.out.println("The News Conference is over");
 		// (The President thread, encountering the bottom of the run() method, will return to it's Thread object and be terminated, which is appropriate!)
