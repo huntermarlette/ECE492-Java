be02cfd214405dc620ccba7645bc4cfa6c8c9a06
huntermarlette <huntermarlette@gmail.com>
2023-04-25 13:14:42 -0400

    project and class file created

---
+++ ECE492_Lab7/NekoTheCat.java
@@ -0,0 +1,19 @@
+// ECE492 Java Lab 7 - Neko The Cat - Spring 2023
+// Hunter Marlette
+// Student ID # 200289830
+
+
+import java.awt.event.MouseListener;
+
+public class NekoTheCat implements MouseListener {
+
+	public NekoTheCat() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
